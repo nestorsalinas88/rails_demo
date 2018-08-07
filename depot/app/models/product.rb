8fdbec7b5fbe2e5314d1f419214a24b234fa8713
nestor salinas <nestorasm88@gmail.com>
2018-08-07 15:13:31 -0500

    testing cart features

--- depot/app/models/product.rb
+++ depot/app/models/product.rb
@@ -1,9 +1,26 @@
 class Product < ApplicationRecord
-  validates :title, :description, :image_url, presence: true
+ has_many :line_items
+
+  before_destroy :ensure_not_referenced_by_any_line_item
+
+  private
+
+  def ensure_not_referenced_by_any_line_item
+    unless line_items.empty?
+      errors.add(:base, 'Line Items present')
+      throw :abort
+    end
+  end
+end
+
+=begin
+ # dont delete
+ validates :title, :description, :image_url, presence: true
   validates :price, numericality: {greater_than_or_equal_to: 0.01}
   validates :title, uniqueness: true
   validates :image_url, allow_blank: true, format: {
       with: %r{\.(gif|jpg|png)\Z}i,
       message: 'Must be a URL for GIF, JPG, or PNG image'
   }
-end
+
+=end
