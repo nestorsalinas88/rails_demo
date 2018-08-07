858ea0e6ae930b148036eba74d807f7dff93c4fd
nestor salinas <nestorasm88@gmail.com>
2018-08-07 10:51:38 -0500

    created: test all successful

--- depot/app/controllers/products_controller.rb
+++ depot/app/controllers/products_controller.rb
@@ -28,9 +28,11 @@ class ProductsController < ApplicationController
 
     respond_to do |format|
       if @product.save
-        format.html { redirect_to @product, notice: 'Product was successfully created.' }
+        format.html { redirect_to @product,
+        notice: 'Product was successfully created.'}
         format.json { render :show, status: :created, location: @product }
       else
+        puts @product.errors.full_messages
         format.html { render :new }
         format.json { render json: @product.errors, status: :unprocessable_entity }
       end
